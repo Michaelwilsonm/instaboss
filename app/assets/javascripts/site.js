45d49ed869c5b9cb96c1f42b8834e6e0733cd3f8
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-04-27 01:06:31 +1200

    add navigation and working on footer

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
     $("h1").hover(function(){
-        $(this).next().stop().fadeIn(500);
+        $(this).next().stop().css({opacity: 0, visibility: "visible"}).animate({opacity: 1}, 500)
     },function(){
-        $(this).next().stop().fadeOut(500);
+        $(this).next().stop().css({opacity: 1, visibility: "visible"}).animate({opacity: 0}, 500)
     });
 });
+

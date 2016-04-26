145761449367ca6c088369f31046238fc9641150
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-04-27 11:36:25 +1200

    add womens and mens templates with routes

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,8 +1,18 @@
 $(document).ready(function(){
-    $("h1").hover(function(){
-        $(this).next().stop().css({opacity: 0, visibility: "visible"}).animate({opacity: 1}, 500)
+    $(".col-md-3").hover(function(){
+        $(this).find("img").stop().css({
+          opacity: 0,
+          visibility: "visible"
+        }).animate({
+          opacity: 1
+        }, 500)
     },function(){
-        $(this).next().stop().css({opacity: 1, visibility: "visible"}).animate({opacity: 0}, 500)
+        $(this).find("img").stop().css({
+          opacity: 1,
+          visibility: "visible"
+        }).animate({
+          opacity: 0
+        }, 500)
     });
 });
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  get 'men/index'
+
+  get 'women/index'
+
   devise_for :users
   root "site#index"
+
+  get 'women' => 'women#index'
+  get 'men' => 'men#index'
+  get 'kid' => 'kid#index'
+  get 'sale' => 'sale#index'
 
 end

90d216c4b84a5ec4d0033147b9dfb39a516edae8
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-05-02 22:54:01 +1200

    add admin login

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -1,7 +1,10 @@
 class ItemController < ApplicationController
   before_action :authenticate_user!
+  before_action :authenticate_admin!
+
 
   def index
+    @item = current_user.fashion_items.all
   end
 
   def new
@@ -11,16 +14,28 @@ class ItemController < ApplicationController
   def create
     @item = current_user.fashion_items.build(fashion_item_params)
     if @item.save
-      redirect_to root_path
+      render 'index'
     else
       render 'new'
     end
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+
   end
 
   def update
   end
 
   private
+
+    def find_item
+    end
 
     def fashion_item_params
      params.require(:FashionItem).permit(:sex, :brand, :description, :category, :sub_category, :price, :sale, :item_image, :shipped_from, :user_id, :sale_price)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  devise_for :admins
+  get 'admin/index'
+
   get 'item/index'
   get 'site/about'
   get 'site/contact'

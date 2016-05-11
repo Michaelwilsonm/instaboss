da079c1c1129e64968f501f98aa918ea593e1c70
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-05-11 23:12:20 +1200

    new card styling complete

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -3,17 +3,11 @@ $(document).ready(function() {
 
     $(".box").hover(function(){
         var $description = $(".description_content")
-        var $spec_table = $(".specials_table>td:nth-child(1)")
-        $(this).find($spec_table).stop().css({opacity: 0, visibility: "visible"}).animate({opacity: 1.0}, 400)
         $(this).find($description).stop().css({opacity: 0, visibility: "visible"}).animate({opacity: 1.0}, 400)
     }, function(){
         var $description = $(".description_content")
-        var $spec_table = $(".specials_table>td:nth-child(1)")
-        $(this).find($spec_table).stop().animate({opacity: 0}, 400);
         $(this).find($description).stop().animate({opacity: 0}, 400);
     });
-
-
   });
 });
 

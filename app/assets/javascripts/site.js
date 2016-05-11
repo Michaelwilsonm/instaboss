d53806ba5df679c3302e5fee3f589fbe6bf51393
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-05-11 23:33:02 +1200

    add new cards to all pages

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   $(document).on('page:change',function() {
-
     $(".box").hover(function(){
         var $description = $(".description_content")
         $(this).find($description).stop().css({opacity: 0, visibility: "visible"}).animate({opacity: 1.0}, 400)

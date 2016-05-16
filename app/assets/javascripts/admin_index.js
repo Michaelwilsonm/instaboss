3e90cd3aca03dc3f6aec281a644028d733d2cae8
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-05-16 22:53:56 +1200

    add featured ability

---
+++ app/assets/javascripts/admin_index.js
@@ -0,0 +1,31 @@
+// $(document).ready(function() {
+//   $(document).on('page:change',function() {
+
+//     $(".featured-button").click(function(e){
+//       e.preventDefault();
+//       e.stopPropagation();
+//       var href = $(this).attr("href")
+//       var id = $(this).attr("id")
+//       console.log(id)
+//       console.log(href)
+
+//     $.ajax({
+//       type: "POST",
+//       url: href,
+//       data: true,
+//       success: function(data){
+//       },
+//       error: function(error){
+//       }
+//     })
+//     })
+
+
+
+
+
+
+
+
+//   });
+// });

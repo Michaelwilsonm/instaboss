64a119738c61b3de37606f89ce8c21e873bc3675
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-05-17 17:31:35 +1200

    navigation drop down complete

--- app/assets/javascripts/item_navigation_top.js
+++ app/assets/javascripts/item_navigation_top.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
   $(document).on('page:change',function() {
 
+    $(".item_nav_top").hover(function(){
+      $(".item_drop_shop_by").show(100)
+    },function(){
+      $(".item_drop_shop_by").hide(100)
+    })
+
 
 
   });

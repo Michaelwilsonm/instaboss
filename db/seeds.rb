309a28b6fb9d5f918d7d189c7c3e36e0c319fc69
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-07-07 22:58:25 +1200

    change route for users when logged in

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,12 +15,12 @@ User.create(email: "mike@mike.com", password: "hello123")
 User.create(email: "mike@email.com", password: "hello123")
 Admin.create(email: "josh@josh.com", password: "hello123")
 
-45.times do
+25.times do
 
   FashionItem.create(
     item_image: img,
     unique_affiliate_url: "www.google.com",
-    user_id: 4,
+    user_id: 1,
     shipped_from: "USA",
     sex: "Mens",
     brand: ['MVMT', 'Morepork', 'just another fisherman', 'timberland'].sample,
@@ -31,16 +31,17 @@ Admin.create(email: "josh@josh.com", password: "hello123")
     short_description: "Too awesome to say",
     sale: [true, false].sample,
     featured_item: [true, false].sample,
+    staff_picks: [true, false].sample,
     wwshipping: [true, false].sample,
     )
 end
 
-5.times do
+25.times do
 
   FashionItem.create(
     item_image: img,
     unique_affiliate_url: "www.google.com",
-    user_id: 3,
+    user_id: 2,
     shipped_from: "USA",
     sex: "Womens",
     brand: ['MVMT', 'Morepork', 'just another fisherman', 'timberland'].sample,

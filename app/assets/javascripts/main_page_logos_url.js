c31ef8972c5dbe07ce4b9d5059c8f5a8e70464cf
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-08-20 16:50:48 +1200

    assets precompile

---
+++ app/assets/javascripts/main_page_logos_url.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $(document).on('page:change',function() {
+
+    // $(".rectangle_left>img").click(function(){
+    //   var brand = $(this).attr("alt")
+    //   window.location.replace("http://www.instaboss.co/men_brand/" + brand)
+    // });
+
+    // $(".rectangle_right>img").click(function(){
+    //   var brand = $(this).attr("alt")
+    //   window.location.replace("http://www.instaboss.co/women_brand/" + brand)
+    // });
+
+
+
+    });
+  });

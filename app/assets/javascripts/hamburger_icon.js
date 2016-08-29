99aba854158a04561f20286ae15a7b5d133ab821
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-08-29 21:09:22 +1200

    add hamburger to mens pages

--- app/assets/javascripts/hamburger_icon.js
+++ app/assets/javascripts/hamburger_icon.js
@@ -1,31 +1,25 @@
 $(document).ready(function() {
   $(document).on('page:change',function() {
+    var toggleNavCount = 0
 
     document.querySelector( "#nav-toggle" )
     .addEventListener( "click", function() {
       this.classList.toggle( "active" );
     });
 
-    var count = 0
     $("#nav-toggle").click(function(){
-    count ++
+    toggleNavCount ++
     $(".hamburger-slider").show()
-      if (count % 2 == 0) {
+      if (toggleNavCount % 2 == 0) {
         $(".hamburger-slider").animate({
-          right: "1000px"},
-          500, function() {
-            console.log("right")
-        });
-      }else if (count % 2 != 0) {
+          right: "1000px"
+        },300)
+      }else if (toggleNavCount % 2 != 0) {
         $(".hamburger-slider").animate({
-          right: "0px"},
-          500, function() {
-            console.log("0 px")
-        });
+          right: "-10px"
+        },300)
       }
     })
-
-
 
 
 

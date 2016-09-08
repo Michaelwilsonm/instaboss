e37155b36b9d51eadf98f8f698b53cbadd2a4ba3
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-09-08 15:22:57 +1200

    add home button to side nav desktop

--- public/assets/application-5b3ef98bdc7ca1c2b85c46aa90c7afb4eac68af6f180be14d0633b76e2eea47a.js
+++ public/assets/application-5b3ef98bdc7ca1c2b85c46aa90c7afb4eac68af6f180be14d0633b76e2eea47a.js
@@ -13240,32 +13240,28 @@ $(document).ready(function() {
 // });
 $(document).ready(function() {
   $(document).on('page:change',function() {
+    var toggleNavCount = 0
 
     document.querySelector( "#nav-toggle" )
     .addEventListener( "click", function() {
       this.classList.toggle( "active" );
     });
 
-    var count = 0
     $("#nav-toggle").click(function(){
-    count ++
+    toggleNavCount ++
     $(".hamburger-slider").show()
-      if (count % 2 == 0) {
+      if (toggleNavCount % 2 == 0) {
+        $(".logo-image").fadeIn()
         $(".hamburger-slider").animate({
-          right: "1000px"},
-          500, function() {
-            console.log("right")
-        });
-      }else if (count % 2 != 0) {
+          right: "1000px"
+        },300)
+      }else if (toggleNavCount % 2 != 0) {
+        $(".logo-image").fadeOut()
         $(".hamburger-slider").animate({
-          right: "0px"},
-          500, function() {
-            console.log("0 px")
-        });
+          right: "-5px"
+        },300)
       }
     })
-
-
 
 
 

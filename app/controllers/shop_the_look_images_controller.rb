d3931c2ffe885f4406a115654544af15bd0ae0fb
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-09-19 02:10:15 +1200

    add form for image with multiple items

--- app/controllers/shop_the_look_images_controller.rb
+++ app/controllers/shop_the_look_images_controller.rb
@@ -8,4 +8,18 @@ class ShopTheLookImagesController < ApplicationController
     3.times { @image_item.shop_the_look_items.build }
   end
 
+  def create
+    @item = current_user.shop_the_look_images.build(params.require(:shop_the_look_image).permit(:shop_look_image, :user_id))
+    @item.save!
+    params["shop_items"].each do |item|
+      @item.shop_the_look_items.create(item_params(item))
+    end
+  end
+
+  private
+
+    def item_params(my_params)
+      my_params.permit(:gender, :brand)
+    end
+
 end
--- app/models/shop_the_look_image.rb
+++ app/models/shop_the_look_image.rb
@@ -1,7 +1,16 @@
 class ShopTheLookImage < ActiveRecord::Base
+  attr_accessor :task_attributes
   belongs_to :user
-  has_many :shop_the_look_items, dependent: :destroy
+  has_many :shop_the_look_items
   has_attached_file :shop_look_image, styles: { original: "600x600>", medium: "300x300>", thumb: "100x100>"}
   validates_attachment_content_type :shop_look_image, content_type: /\Aimage\/.*\Z/
 
+
+  def task_attributes=(task_attributes)
+    task_attributes.each do |items|
+      shop_the_look_items.build(items)
+    end
+  end
 end
+
+{"utf8"=>"✓", "authenticity_token"=>"HA+ugZXtnO2W/vJugpMCqBlEOhj/40jUPm7pwU+aXuG7eMSUrF1oPGE+R557AO6M4mUEUF5Z2D3J1TiEeLBFPw==", "shop_the_look_image"=>{"task_attributes"=>[{"brand"=>"dsadsa", "gender"=>"dsa"}, {"brand"=>"dsa", "gender"=>""}, {"brand"=>"", "gender"=>""}]}, "commit"=>"create"}

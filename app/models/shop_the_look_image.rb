1b50236f5b78458bd043bca079da8531474496ee
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-09-21 22:18:18 +1200

    destory function

--- app/models/shop_the_look_image.rb
+++ app/models/shop_the_look_image.rb
@@ -5,8 +5,7 @@ class ShopTheLookImage < ActiveRecord::Base
   has_attached_file :shop_look_image, styles: { original: "600x600>", medium: "300x300>", thumb: "100x100>"}
   validates_attachment_content_type :shop_look_image, content_type: /\Aimage\/.*\Z/
 
-  accepts_nested_attributes_for :shop_the_look_items, reject_if: :all_blank
-  # reject_if: proc { |a| a[:sub_category, :category, :brand, :price, :sale, :unique_affiliate_url].blank? }
+  accepts_nested_attributes_for :shop_the_look_items, reject_if: :all_blank, allow_destroy: true
 
   def self.shop_the_look_women
     ShopTheLookImage.find_by_sql("SELECT * FROM shop_the_look_images WHERE gender = 'Womens' ORDER BY created_at DESC")

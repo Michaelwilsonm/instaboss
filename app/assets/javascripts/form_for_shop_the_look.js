56a2590f19cd5f5945f9651182eea59e4e887f3c
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-09-27 10:50:11 +1300

    form for men and women cat working

--- app/assets/javascripts/form_for_shop_the_look.js
+++ app/assets/javascripts/form_for_shop_the_look.js
@@ -1,45 +1,14 @@
 $(document).ready(function() {
   $(document).on('page:change', function () {
-
-    // if (!Array.prototype.last){
-    //   Array.prototype.last = function(){
-    //       return this[this.length - 1];
-    //   };
-    // };
-
     womensLookCategories = {'Accessories': ['Lingerie & Underwear', 'Hats', 'Bags & Purses', 'Jewellery', 'Other'], 'Dresses': ['Day Dresses', 'Evening Dresses'], 'Jeans Trousers & Leggings': ['Jeans', 'Trousers','Leggings'], 'Home & Lifestyle': ['Home & Living'], 'Shoes': ['Boots', 'Trainers', 'Heels', 'Flats'], 'Shorts & Skirts': ['Shorts', 'Skirts'], 'Sunglasses & Watches': ['Sunglasses', 'Watches'], 'Swim & Beachwear': ['Bikinis', 'Swimsuits', 'Other'], 'Tops': ['T-Shirts & Vests', 'Shirts & Blouses', 'Hoodies & Sweatshirts', 'Jackets & Coats'] };
 
     mensLookCategories = {'Accessories': ['Hats', 'Bags', 'Jewellery', 'Other'], 'Formal': ['Jackets & Coats','Suits', 'Accessories'], 'Lifestyle': ['Lifestyle'], 'Longs': ['Jeans', 'Chinos', 'Joggers'], 'Shoes': ['Boat & Loafers', 'Boots', 'Trainers', 'Formal', 'Other'], 'Shorts & Swimwear': ['Shorts', 'Swimwear'], 'Sunglasses & Watches': ['Sunglasses', 'Watches'] , 'Tops': ['T-Shirts & Polos', 'Shirts', 'Hoodies & Sweatshirts', 'Jackets & Coats'] };
 
-    // $(".gender-look").change(function(){
-    //     $(".cat-look").children().remove()
-    //     $(".sub-cat-look").children().remove()
-    //   if ( $(this).val() == 'Mens' ){
-    //     categories(mensLookCategories)
-    //   } else if ( $(this).val() == 'Womens' ){
-    //     categories(womensLookCategories)
-    //   }
-    // });
-
-
-    // function changeCategories (_this, findCatClass, findSubCatClass){
-    //   if (_this.val() === $("." + findCatClass).val() && $(".gender-look").val() =='Mens') {
-    //     $("." + findSubCatClass).children().remove()
-    //     subCategories(findCatClass, findSubCatClass, mensLookCategories)
-    //   } else if (_this.val() === $("." + findCatClass).val() && $(".gender-look").val() == 'Womens') {
-    //     $("." + findSubCatClass).children().remove()
-    //     subCategories(findCatClass, findSubCatClass, womensLookCategories)
-    //   }
-    // }
-
-    function categories(itemCategories, thisForm){
-      if ($(".gender-look").val() == "Mens") {
-        thisForm.append("<option value="+">"+"</option>");
-        $.each( itemCategories, function( key, value ) {
-          thisForm.append("<option value=" + key + ">"  +  key + "</option>");
-        });
-      }
-    }
+    if (!Array.prototype.last){
+      Array.prototype.last = function(){
+          return this[this.length - 1];
+      };
+    };
 
     $('#form-for-shop-look')
       .on('cocoon:before-insert', function(e,task_to_be_added) {
@@ -47,17 +16,57 @@ $(document).ready(function() {
       })
       .on('cocoon:after-insert', function(e, added_task) {
         var thisForm = added_task.find(".cat-look")
-        categories(mensLookCategories, thisForm)
+        categories(mensLookCategories, womensLookCategories, thisForm)
+
         thisForm.change(function(){
           var catVal = $(this).val()
           var subCat = $(this).next().next();
-          subCategories(subCat, catVal, mensLookCategories)
+          subCategories(subCat, catVal, mensLookCategories, womensLookCategories)
         })
       })
 
-    function subCategories(subCat, catVal, menCat){
+    $(".gender-look").change(function(){
+        $(".cat-look").children().remove()
+        $(".sub-cat-look").children().remove()
+      if ( $(this).val() == 'Mens' ){
+        genderChange(mensLookCategories)
+      } else if ( $(this).val() == 'Womens' ){
+        genderChange(womensLookCategories)
+      }
+    });
+
+    function genderChange(catMenWomen) {
+      $(".cat-look").append("<option value="+">"+"</option>");
+      $.each( catMenWomen, function( key, value ) {
+        $(".cat-look").append("<option value=" + key + ">"  +  key + "</option>");
+      });
+    }
+
+    function categories(menCat, womenCat, thisForm){
+      if ($(".gender-look").val() == "Mens") {
+        thisForm.append("<option value="+">"+"</option>");
+        $.each( menCat, function( key, value ) {
+          thisForm.append("<option value=" + key + ">"  +  key + "</option>");
+        });
+      } else if ($(".gender-look").val() == "Womens") {
+        thisForm.append("<option value="+">"+"</option>");
+        $.each( womenCat, function( key, value ) {
+          thisForm.append("<option value=" + key + ">"  +  key + "</option>");
+        });
+      }
+    }
+
+    function subCategories(subCat, catVal, menCat, womenCat){
       subCat.children().remove()
-      $.each( menCat, function( key, value ) {
+      if ($(".gender-look").val() == 'Mens') {
+        findSubCat(menCat, catVal, subCat)
+      } else if ($(".gender-look").val() == 'Womens') {
+        findSubCat(womenCat, catVal, subCat)
+      }
+    }
+
+    function findSubCat(catMenWomen, catVal, subCat) {
+      $.each( catMenWomen, function( key, value ) {
         if (key.includes(catVal)) {
           var subCategory = value;
           $.each( subCategory, function( index, subCatValues ) {
@@ -67,25 +76,6 @@ $(document).ready(function() {
       });
     }
 
-
-
-    // var textCount = 0
-    // $(".add-shop-look-item").click(function(){
-    //   textCount ++
-    //   $(".form-3").toggle(100)
-    //   if (textCount % 2 == 0) {
-    //     $(this).text("Remove Item");
-    //     $(this).addClass('btn-danger').removeClass('btn-success')
-    //   } else {
-    //     $(this).text("Add Item");
-    //     $(this).addClass('btn-success').removeClass('btn-danger')
-    //     $(".new-item-look-category-3").children().first().remove()
-    //     $(".new-item-look-category-3").prepend("<option value=" + "" +">" + "" + "</option>")
-    //     $(".new-item-look-sub-category-3").children().first().remove()
-    //     $(".new-item-look-sub-category-3").prepend("<option value=" + "" +">" + "" + "</option>")
-    //     $('.item-shop-the-look-3').val('');
-    //   }
-    // })
 
   });
 });

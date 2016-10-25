be2ed41b6e33effe66c1433c43239246a8e0cbcf
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-10-25 20:52:00 +1300

    change featured items sizes to a bigger size

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -8,5 +8,13 @@ $(document).ready(function() {
         $(this).find($description).stop().animate({opacity: 0}, 400);
     });
 
+    $(".image_div_featured").hover(function(){
+        var $description = $(".description_content_featured")
+        $(this).find($description).stop().css({opacity: 0, visibility: "visible"}).animate({opacity: 1.0}, 400)
+    }, function(){
+        var $description = $(".description_content_featured")
+        $(this).find($description).stop().animate({opacity: 0}, 400);
+    });
+
   });
 });

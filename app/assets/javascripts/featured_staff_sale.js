f3f646283d92d661825d34737c18a1a72c7f77e1
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-11-23 20:40:11 +1300

    fix bug for scrolling only on men and womens features

--- app/assets/javascripts/featured_staff_sale.js
+++ app/assets/javascripts/featured_staff_sale.js
@@ -7,18 +7,29 @@ $(document).ready(function() {
       var womensPage = $(".womens_main_photo").hasClass('women-page');
       var mensPage = $(".mens_main_photo").hasClass('men-page');
 
-      if (scrollHeight < 1685){
-        if (mensPage){
-        $(".desktop-categories>h1").text("Featured Men's");
-        } else if (womensPage){
-          $(".desktop-categories>h1").text("Featured Women's");
+      if (mensPage){
+        if (scrollHeight < 1685){
+          $(".desktop-categories>h1").text("Featured Men's");
+        } else if (scrollHeight > 1685 && scrollHeight < 2942) {
+          $(".desktop-categories>h1").text("Staff Picks");
+        } else if (scrollHeight > 2942) {
+          $(".desktop-categories>h1").text("Featured Sale");
         }
-      } else if (scrollHeight > 1685 && scrollHeight < 2942) {
-        $(".desktop-categories>h1").text("Staff Picks");
-      } else if (scrollHeight > 2942) {
-        $(".desktop-categories>h1").text("Featured Sale");
       }
+
+      if (womensPage){
+          if (scrollHeight < 1685){
+            $(".desktop-categories>h1").text("Featured Women's");
+          } else if (scrollHeight > 1685 && scrollHeight < 2942) {
+            $(".desktop-categories>h1").text("Staff Picks");
+          } else if (scrollHeight > 2942) {
+            $(".desktop-categories>h1").text("Featured Sale");
+          }
+        }
+
+
     })
+
 
 
   })

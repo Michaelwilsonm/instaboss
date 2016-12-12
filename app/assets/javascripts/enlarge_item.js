3635f86e8c23e3550038f317308c15ac8858db47
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-12-12 23:01:39 +1300

    add enlarging on click to all templates

--- app/assets/javascripts/enlarge_item.js
+++ app/assets/javascripts/enlarge_item.js
@@ -9,6 +9,13 @@ $(document).ready(function() {
       $(this).next().fadeIn(500)
     });
 
+    $(".box_featured").click(function(e){
+      e.preventDefault()
+      $(".on-click-enlarge").hide()
+      $(".tint").fadeIn(500)
+      $(this).next().fadeIn(500)
+    });
+
     $(".close-button-enlarged").click(function(){
       $(".on-click-enlarge").fadeOut(500)
       $(".tint").fadeOut(500)

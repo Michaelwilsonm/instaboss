65061087a9dc5a7735d090784ec7b2a5ba50a4f2
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-12-12 14:10:54 +1300

    remove click ability from search function

--- app/assets/javascripts/search_input.js
+++ app/assets/javascripts/search_input.js
@@ -1,18 +1,4 @@
 $(document).ready(function() {
   $(document).on('page:change',function() {
-
-    jQuery.fn.clickToggle = function(a,b) {
-      function cb(){ [b,a][this._tog^=1].call(this); }
-      return this.on("click", cb);
-    };
-
-    $(".search-btn>i").clickToggle(function(e){
-      $(".search-btn").animate({marginRight: "190px"}, 500)
-      $(".search-input").animate({marginRight: "20px"}, 500)
-    }, function(){
-      $(".search-btn").animate({marginRight: "20px"}, 500)
-      $(".search-input").animate({marginRight: "-160px"}, 500)
-    })
-
   });
 });

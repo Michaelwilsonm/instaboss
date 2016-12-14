39255b4f5171ecefcaef9acc56fc1f7665d6b2f8
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2016-12-14 14:51:23 +1300

    add screen width for mobile only

--- app/assets/javascripts/enlarge_item.js
+++ app/assets/javascripts/enlarge_item.js
@@ -2,29 +2,34 @@ $(document).ready(function() {
   $(document).on('page:change',function() {
 
 
-    $(".box").click(function(e){
-      e.preventDefault()
-      $(".on-click-enlarge").hide()
-      $(".tint").fadeIn(500)
-      $(this).next().fadeIn(500)
-    });
+    if (screen.width > 770){
+      $(".box").click(function(e){
+        console.log(e)
+        e.preventDefault()
+        $(".on-click-enlarge").hide()
+        $(".tint").fadeIn(500)
+        $(this).next().fadeIn(500)
+      });
 
-    $(".box_featured").click(function(e){
-      e.preventDefault()
-      $(".on-click-enlarge").hide()
-      $(".tint").fadeIn(500)
-      $(this).next().fadeIn(500)
-    });
+      $(".box_featured").click(function(e){
+        e.preventDefault()
+        $(".on-click-enlarge").hide()
+        $(".tint").fadeIn(500)
+        $(this).next().fadeIn(500)
+      });
 
-    $(".close-button-enlarged").click(function(){
-      $(".on-click-enlarge").fadeOut(500)
-      $(".tint").fadeOut(500)
-    });
+      $(".close-button-enlarged").click(function(){
+        $(".on-click-enlarge").fadeOut(500)
+        $(".tint").fadeOut(500)
+      });
 
-    $(".tint").click(function(){
-      $(".on-click-enlarge").fadeOut(500)
-      $(".tint").fadeOut(500)
-    });
+      $(".tint").click(function(){
+        $(".on-click-enlarge").fadeOut(500)
+        $(".tint").fadeOut(500)
+      });
+    }
+
+
 
 
 

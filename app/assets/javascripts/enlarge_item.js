b9dab29cb902cc331b95a6c576b900136d29f92c
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2017-02-16 10:54:38 +1300

    remove on change function

--- app/assets/javascripts/enlarge_item.js
+++ app/assets/javascripts/enlarge_item.js
@@ -1,32 +1,29 @@
 $(document).ready(function() {
-  $(document).on('page:change',function() {
 
-    THIS_PAGE_URL = window.location.href
+  THIS_PAGE_URL = window.location.href
 
-    if (screen.width > 770){
+  if (screen.width > 770){
+    $(document).on( "click", ".close-button-enlarged", function(e) {
+      window.history.pushState('page2', 'Title', THIS_PAGE_URL);
+      $(".outside-container").fadeOut(500)
+      $(".tint").fadeOut(500)
+    });
 
-      $(document).on( "click", ".close-button-enlarged", function(e) {
-        window.history.pushState('page2', 'Title', THIS_PAGE_URL);
-        $(".outside-container").fadeOut(500)
-        $(".tint").fadeOut(500)
-      });
+    $(document).on( "click", ".box", function(e) {
+      e.preventDefault()
+      var href = $(this).find("a").attr('href');
+      window.history.pushState('page2', 'Title', href);
+      $(".outside-container").hide()
+      $(this).next().fadeIn(500)
+      $(".tint").fadeIn(500)
+    });
 
-      $(document).on( "click", ".box", function(e) {
-        e.preventDefault()
-        var href = $(this).find("a").attr('href');
-        window.history.pushState('page2', 'Title', href);
-        $(".outside-container").hide()
-        $(this).next().fadeIn(500)
-        $(".tint").fadeIn(500)
-      });
-
-    }
-      $(document).on( "click", ".tint", function(e) {
-        window.history.pushState('page2', 'Title', THIS_PAGE_URL);
-        $(".outside-container").fadeOut(500)
-        $(".tint").fadeOut(500)
-      });
+    $(document).on( "click", ".tint", function(e) {
+      window.history.pushState('page2', 'Title', THIS_PAGE_URL);
+      $(".outside-container").fadeOut(500)
+      $(".tint").fadeOut(500)
+    });
+  }
 
 
-  });
 });

684d79dcd72b674b16d5f4890b9d79462b59fcaa
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2017-02-16 10:39:55 +1300

    change selectors for jquery for after load on pagination

--- app/assets/javascripts/enlarge_item.js
+++ app/assets/javascripts/enlarge_item.js
@@ -2,31 +2,30 @@ $(document).ready(function() {
   $(document).on('page:change',function() {
 
     THIS_PAGE_URL = window.location.href
-    var $outsideContainer = $(".outside-container")
-    var $tint = $(".tint")
 
     if (screen.width > 770){
+
       $(document).on( "click", ".close-button-enlarged", function(e) {
         window.history.pushState('page2', 'Title', THIS_PAGE_URL);
-        $outsideContainer.fadeOut(500)
-        $tint.fadeOut(500)
+        $(".outside-container").fadeOut(500)
+        $(".tint").fadeOut(500)
       });
 
       $(document).on( "click", ".box", function(e) {
         e.preventDefault()
         var href = $(this).find("a").attr('href');
         window.history.pushState('page2', 'Title', href);
-        $outsideContainer.hide()
+        $(".outside-container").hide()
         $(this).next().fadeIn(500)
-        $tint.fadeIn(500)
+        $(".tint").fadeIn(500)
       });
 
+    }
       $(document).on( "click", ".tint", function(e) {
         window.history.pushState('page2', 'Title', THIS_PAGE_URL);
-        $outsideContainer.fadeOut(500)
-        $tint.fadeOut(500)
+        $(".outside-container").fadeOut(500)
+        $(".tint").fadeOut(500)
       });
-    }
 
 
   });

12121db837b3d43cbdc7da99823b531b05fa9c9d
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2017-02-15 17:11:09 +1300

    add formal js erb files for pagination

--- app/controllers/sub_cat_men/formal_controller.rb
+++ app/controllers/sub_cat_men/formal_controller.rb
@@ -4,23 +4,39 @@ class SubCatMen::FormalController < MenController
     @formal_fashion_item = @items.men_categories("Formal")
     @formal_shop_look = @shop_look_images.find_category_mens("Formal").uniq!
     join_items_and_sort(@formal_fashion_item, @formal_shop_look)
+    respond_to do |format|
+        format.html
+        format.js
+    end
   end
 
   def jacket_coat
     @formal_fashion_item = @items.men_sub_categories("Jackets")
     @formal_shop_look = @shop_look_images.find_category_mens("Formal").uniq!
     join_items_and_sort(@formal_fashion_item, @formal_shop_look)
+    respond_to do |format|
+        format.html
+        format.js
+    end
   end
 
   def accessory
     @formal_fashion_item = @items.men_sub_categories("Accessories")
     @formal_shop_look = @shop_look_images.find_category_mens("Formal").uniq!
     join_items_and_sort(@formal_fashion_item, @formal_shop_look)
+    respond_to do |format|
+        format.html
+        format.js
+    end
   end
 
   def suit
     @formal_fashion_item = @items.men_sub_categories("Suit")
     @formal_shop_look = @shop_look_images.find_category_mens("Formal").uniq!
     join_items_and_sort(@formal_fashion_item, @formal_shop_look)
+    respond_to do |format|
+        format.html
+        format.js
+    end
   end
 end

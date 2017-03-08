c03e1ab1f68c93c47a16a509235b366cbe80ad25
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2017-03-08 19:00:06 +1300

    sorting category filtering

--- app/controllers/sub_cat_women/home_controller.rb
+++ app/controllers/sub_cat_women/home_controller.rb
@@ -1,8 +1,8 @@
 class SubCatWomen::HomeController < WomenController
 
   def home
-    @home_item = @items.women_categories("Home & Lifestyle")
-    @home_shop_look_item = @shop_look_images.find_category_womens("Home & Lifestyle").uniq!
+    @home_item = @items.women_categories("Home")
+    @home_shop_look_item = @shop_look_images.find_category_womens("Home").uniq!
     join_items_and_sort(@home_item, @home_shop_look_item)
     @all_featured = @items.featured_womens_categorys_four("Accessories").sample(4)
     respond_to do |format|
